257a849bdbab6c657911d905336edcfbe4e09dae
JuniorReis-dev <cleiton.nr47@gmail.com>
2025-05-19 15:04:05 -0300

    :construction: implementaçao dos metodos do crud

--- src/main/java/com/generetions/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/com/generetions/blogpessoal/repository/PostagemRepository.java
@@ -1,9 +1,12 @@
 package com.generetions.blogpessoal.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.generetions.blogpessoal.model.Postagem;
 
 public interface PostagemRepository extends JpaRepository<Postagem , Long>{
 
+	List<Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 }
